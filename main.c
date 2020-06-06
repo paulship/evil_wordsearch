150323d3584f4048cd33b5923139c638340c6a3c
Paul Shipley <paul.shipley@blueyonder.co.uk>
2020-06-06 23:39:52 +0100

    Comment main and minor cleanup.

--- main.c
+++ main.c
@@ -5,35 +5,32 @@
 
 int main( void )
 {
-    printf("Starting evil wordsearch generator.\n");
-
+    /* Initialise the random number generator - only do this once!!! */
     srand(time(NULL));
 
-    wordsearch_t* wordsearch_ptr = wordsearch_ctor("bug",160,60);
+    /* After we have sanitised the inputs, we are able to create our wordsearch. */
+    wordsearch_t* wordsearch_ptr = wordsearch_ctor("BUG",60,20);
 
     if( wordsearch_ptr == NULL )
     {
-        printf("It didn't work.\n");
+        printf("Memory allocation error.\n");
+        return EXIT_FAILURE;
     }
     else
     {
-        wordsearch_fill_blanks( wordsearch_ptr );
+        /* Repeatedly fill in any blanks and remove instances where the target word appears, until it is no longer there. */
         do
         {
-            //printf("\nWordsearch is now:\n");
-            //wordsearch_print( wordsearch_ptr );
             wordsearch_fill_blanks( wordsearch_ptr );
-            //printf("\nRefilled blanks:\n");
-            //wordsearch_print( wordsearch_ptr );
         } while ( wordsearch_blank_words( wordsearch_ptr ) );
 
 
-        printf("\n\nFinal wordsearch now looks like this.....\n\n");
+        /* Output the final wordsearch. */
         wordsearch_print( wordsearch_ptr );
 
+        /* Clean up memory usage. */
         wordsearch_dtor( wordsearch_ptr );
     }
 
-    printf("All done - exiting now.\n");
-    return 0;
+    return EXIT_SUCCESS;
 }
