883dcfcd48f23ecd5b0b417ac893a02cb5e09578
Paul Shipley <paul.shipley@blueyonder.co.uk>
2020-06-07 02:25:16 +0100

    Adding sanity checks and command line control - sanity check for target word is still to come...

--- main.c
+++ main.c
@@ -1,15 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <time.h>
+#include <string.h>
 #include "wordsearch.h"
 
-int main( void )
+/* This is the maximum dimension of either the width or height of the output (ought to be big enough!!) */
+#define MAX_DIMENSIONS  65535
+
+static void output_usage_text( void );
+static bool check_dimension_ok( const char* const input, int* const output );
+static bool check_target_word_ok( const char* const word );
+
+int main( const int argc, const char* const argv[] )
 {
+    int height = 0;
+    int width = 0;
+
     /* Initialise the random number generator - only do this once!!! */
     srand(time(NULL));
 
+    /* Check we have received 3 arguments. */
+    if( argc != 4 )
+    {
+        output_usage_text();
+        return EXIT_FAILURE;
+    }
+
+    /* Check that width and height parameters are sensible and in range. */
+    if( !check_dimension_ok( argv[2], &width ) )
+    {
+        printf( "Invalid width, must be in the range 0-%d.\n", MAX_DIMENSIONS );
+        return EXIT_FAILURE;
+    }
+    if( !check_dimension_ok( argv[3], &height ) )
+    {
+        printf( "Invalid height, must be in the range 0-%d.\n", MAX_DIMENSIONS );
+        return EXIT_FAILURE;
+    }
+
+    /* Check target word is ok. */
+    if( !check_target_word_ok( argv[1] ) )
+    {
+        printf( "Target word invalid - must contain only alphanumerics, and at least 3 unique characters.\n" );
+        return EXIT_FAILURE;
+    }
+
     /* After we have sanitised the inputs, we are able to create our wordsearch. */
-    wordsearch_t* wordsearch_ptr = wordsearch_ctor("BUG",60,20);
+    wordsearch_t* wordsearch_ptr = wordsearch_ctor(argv[1],width,height);
 
     if( wordsearch_ptr == NULL )
     {
@@ -33,4 +71,51 @@ int main( void )
     }
 
     return EXIT_SUCCESS;
+}
+
+
+static void output_usage_text( void )
+{
+    printf( "Evil wordsearch generator.\n"
+            "Usage:\n"
+            "    wordsearch [target word] [width] [height]\n"
+            "\n"
+            "Target word must be alphanumeric, not contains spaces, and at least 3 unique characters.\n"
+            "\n" );
+}
+
+static bool check_dimension_ok( const char* const input, int* const output )
+{
+    /* Check the string input length is ok. */
+    if( strlen( input ) > 6 )
+    {
+        return false;
+    }
+
+    /* Check the string only contains numbers */
+    const char* c_ptr = input;
+    while( *c_ptr != '\0' )
+    {
+        if( ( *c_ptr < '0' ) || ( *c_ptr > '9' ) )
+        {
+            return false;
+        }
+        c_ptr++;
+    }
+
+    /* Convert to integer */
+    *output = atoi( input );
+
+    /* Check it is in range. */
+    if( ( *output < 0 ) || ( *output > MAX_DIMENSIONS )  )
+    {
+        return false;
+    }
+
+    return true;
+}
+
+static bool check_target_word_ok( const char* const word )
+{
+    return( true );
 }
